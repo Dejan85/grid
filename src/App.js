a18c5da4bb1b88c81987394ecee9a2ae715d6c90
dejan markovic <dejan.markovic@dejan.markovic>
2020-03-06 15:00:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+// Grid
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import { gridOptions, defaultSortModel } from './components/gridOptions';
+
+
 function App() {
+  const [rowData, setRowData] = useState();
+
+  const onGridReady = params => {
+    // const gridApi = params.api;
+    // const gridColumnApi = params.columnApi;
+
+    // console.log(gridApi);
+    // console.log(gridColumnApi);
+
+    fetch("https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinnersSmall.json")
+      .then((res) => res.json())
+      .then((response) => {
+        setRowData(response)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+    params.api.setSortModel(defaultSortModel); // when grid load, defaul sorting method
+
+
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ag-theme-balham"
+      style={{ height: '500px', width: "100%" }}
+    >
+      <AgGridReact
+        gridOptions={gridOptions}
+        rowSelection="multiple"
+        onGridReady={onGridReady.bind(this)}
+        rowData={rowData}
+        animateRows={true}
+
+
+      />
     </div>
   );
 }
---
+++ src/components/gridOptions.js
@@ -0,0 +1,52 @@
+
+export const gridOptions = {
+    columnDefs: [
+        {
+            headerName: 'Athlete',
+            field: 'athlete',
+            width: 150,
+        },
+        {
+            headerName: "Age",
+            field: 'age',
+            width: 90
+        },
+        {
+            headerName: "Country",
+            field: 'country',
+            width: 150
+        },
+        {
+            headerName: 'Year',
+            field: 'year',
+            width: 90,
+            // sortingOrder: ["asc", "desc"]
+
+        },
+        {
+            headerName: 'Date',
+            field: 'date',
+            width: 100,
+            unSortIcon: true,
+
+        },
+        {
+            headerName: 'Sport',
+            field: 'sport',
+            width: 150
+        }
+    ],
+    defaultColDef: {
+        sortable: true, // global sort enable
+    },
+    sortingOrder: ["desc", null], // global default method sorting
+
+}
+
+export const defaultSortModel = [
+    {
+        colId: "year",
+        sort: "asc"
+    }
+];
+
